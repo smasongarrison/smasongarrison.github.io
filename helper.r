c67c67a7563bd4b10e5191d6f6afdc1c8a90bcf9
smasongarrison <sarah.m.garrison@vanderbilt.edu>
2016-10-01 13:49:16 -0500

    update

---
+++ helper.r
@@ -0,0 +1,13 @@
+# Compile
+
+## Generate Index Page
+source("generateIndexPage.R")
+
+## Generate Navigation Bar
+source("generateNavigationBar.R")
+
+## Extract Datasets from R
+source("datalists/datasets_extract.r")
+
+## Render Markdown File
+rmarkdown::render(input="Data.Rmd", output_file="Data.html")
