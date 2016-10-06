bf20d6fde41c124c7dc249c4fc9f2682a8937a06
smasongarrison <sarah.m.garrison@vanderbilt.edu>
2016-10-06 11:51:28 -0500

    data

--- helper.r
+++ helper.r
@@ -13,10 +13,12 @@ c=0
 #j=50
 #c=c+1
 #num=c*j+31
+#i=227
 num=i+1
 ## Extract Datasets from R
 source("datalists/datasets_extract.r")
-
+index_out$unique<-paste0(index_out$Package,index_out$Item)
+write.csv(index_out, file='datalists/datasets_import.csv', row.names=FALSE)
 ## Render Markdown Files
 rmarkdown::render(input="Data.Rmd", output_file="Data.html")
 
