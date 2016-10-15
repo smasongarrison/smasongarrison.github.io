2ddc3f9751d72fed444455853c4e748010ee79f8
smasongarrison <sarah.m.garrison@vanderbilt.edu>
2016-10-14 19:08:10 -0500

    more data

--- helper.r
+++ helper.r
@@ -8,8 +8,12 @@ source("generateNavigationBar.R")
 j=50
 #c=c+1
 #num=c*j+31
-i=227
-num=i+1
+#i=0; num=0+i
+
+
+
+
+num=num+j+c(1:length(packages))[packages==package]
 ## Extract Datasets from R
 source("datalists/datasets_extract.r")
 index_out$unique<-paste0(index_out$Package,index_out$Item)
