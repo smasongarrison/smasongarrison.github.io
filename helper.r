44cb99f7d76425979b42c9c1df3b3ec45b1080d8
Mason Garrison <sarah.m.garrison@vanderbilt.edu>
2017-07-11 15:11:26 -0500

    update

--- helper.r
+++ helper.r
@@ -27,6 +27,7 @@ write.csv(index_out, file='datalists/datasets_import.csv', row.names=FALSE)
 ## Render Markdown Files
 rmarkdown::render(input="Data.Rmd", output_file="Data.html")
 rmarkdown::render(input="Class.Rmd", output_file="Class.html")
+rmarkdown::render(input="Class_archive_2017.Rmd", output_file="Class_archive_2017.html")
 #rmarkdown::render(input="AAA.Rmd", output_file="AAA.html")
 
 .rs.restartR()
